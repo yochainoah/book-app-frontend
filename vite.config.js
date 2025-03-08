038f953b5188b243c5e20e19374544b65c67b0e8
yochai heinrich <yochyochai123@gmail.com>
2025-03-08 13:08:04 +0200

    erasing polyfols

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import NodePolyfills from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),NodePolyfills()],
+  plugins: [react()],
 })
